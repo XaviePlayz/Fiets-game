00eddf6888d21ea24db92dc5bc8c607a0b1b5441
Xavier Wevers <xavier.wevers@gmail.com>
2024-02-21 11:29:24 +0100

    Improved all the textures for the game

--- Fiets-game/Assets/_Scripts/Collectibles.cs
+++ Fiets-game/Assets/_Scripts/Collectibles.cs
@@ -31,5 +31,38 @@ public class Collectibles : MonoBehaviour
                 audioSource.PlayOneShot(collectSound);
             }
         }
+        if (other.CompareTag("Pedal"))
+        {
+            other.gameObject.SetActive(false);
+            Debug.Log("PEDAL Collected!");
+
+            // Play the collect sound
+            if (collectSound != null && audioSource != null)
+            {
+                audioSource.PlayOneShot(collectSound);
+            }
+        }
+        if (other.CompareTag("Back_Wheel"))
+        {
+            other.gameObject.SetActive(false);
+            Debug.Log("BACK WHEEL Collected!");
+
+            // Play the collect sound
+            if (collectSound != null && audioSource != null)
+            {
+                audioSource.PlayOneShot(collectSound);
+            }
+        }
+        if (other.CompareTag("HandleBar"))
+        {
+            other.gameObject.SetActive(false);
+            Debug.Log("HANDLEBAR Collected!");
+
+            // Play the collect sound
+            if (collectSound != null && audioSource != null)
+            {
+                audioSource.PlayOneShot(collectSound);
+            }
+        }
     }
 }
